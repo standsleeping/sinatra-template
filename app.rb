f03fbe03a227d430f61d0cc44a870ae658905a43
standsleeping <standsleeping@gmail.com>
2021-06-21 13:05:42 -0500

    Update

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
-set :database, { adapter: 'sqlite3', database: 'db/hehe.sqlite3' }
+# set :database, { adapter: 'sqlite3', database: 'db/testdb.sqlite3' }
 
 get '/' do
   'Hello'
+end
+
+# curl -X POST http://localhost:4567/tbd
+post '/tbd' do
+  'World'
 end
